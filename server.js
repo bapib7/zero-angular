77e8e08de45369dae326575f6b931654281e6fd8
rakeshkumar.biswas <rakeshkumar.biswas@webskitters.com>
2020-02-26 19:43:58 +0530

    changed

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/zero-angular/dist'));
+app.use(express.static(__dirname + '/dist/zero-angular/'));
 
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/zero-angular/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/zero-angular/index.html'));
   });
 // Start the app by listening on the default
 // Heroku port
